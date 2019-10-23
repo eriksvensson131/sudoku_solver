673744096e2048c8641e756265e0f472161fa878
Erik Svensson <erik_svens_son@hotmail.com>
2019-10-23 10:29:04 +0200

    added argparse

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,17 @@
 import sudoku_puzzles
 import SudokuSolver
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Solve a sudoku')
+parser.add_argument('puzzleNumber', metavar='puzzle', type=str,
+                    help='select puzzle1, puzzle2, puzzle3 or puzzle4')
+args = parser.parse_args()
 
 puzzles = sudoku_puzzles.puzzle_dict
-ss = SudokuSolver.SudokuSolver(puzzles['puzzle3'])
+board = puzzles[args.puzzleNumber]
+
+ss = SudokuSolver.SudokuSolver(board)
 
 print(f'The original puzzle looks like:\n{ss.board}\n')
 ss.solve()
